235ccdfb3f69278ea13f602f42683050bebf7411
RRoyStudio <roy.rebecca98@gmail.com>
2024-11-20 13:38:48 -0600

    still testing

--- src/Components/Business.js
+++ src/Components/Business.js
@@ -3,55 +3,61 @@ import InsideCraft from "./assets/InsideCraft.jpg";
 import "./Business.css";
 
 const image= [InsideCraft,]
-const name= ['Craft Beers', 'Inside Craft']
-const address= ['This is the Address', 'address 2']
-const phone= ['(000) 000-0000','(000) 000-0000']
-const category= ['Bar and Grill', 'Bar and Grill']
-const rating= ['5/5', '5/5']
-const reviewCount= ['1000', '1000']
+const name= ['Craft Beers', ]
+const address= ['This is the Address',]
+const phone= ['(000) 000-0000',]
+const category= ['Bar and Grill', ]
+const rating= ['5/5',]
+const reviewCount= ['1000', ]
+ 
+// Create an array of a single business
+const businessItems = image.map((image, index) => ({
+ image,
+ name: name[index],
+ address: address[index],
+ phone: phone[index],
+ category: category[index],
+ rating: rating[index],
+ reviewCount: reviewCount[index],
 
+}));
 
-function BusinessImage() {
-    return <img src= {image} className="Business-image" />
+// Create a component to display a single business
+function BusinessCard({image, name, address, phone, category, rating, reviewCount}) {
+  return (
+    <div className="Business-Card">
+      <img src={image} alt="Business" />
+      <h1>{name}</h1>
+      <p>{address}</p>
+      <p>{phone}</p>
+      <p>{category}</p>
+      <p>{rating}</p>
+      <p>{reviewCount}</p>
+    </div>
+  );
 }
-function BusinessNames() {
-    return <h1 className="Business-name">{name}</h1>
+
+// Create a component to display a list of businesses
+function BusinessList() {
+  return (
+    <div>
+      {businessItems.map((item, index) => (
+        <BusinessCard
+          key={index}
+          image={item.image}
+          name={item.name}
+          address={item.address}
+          phone={item.phone}
+          category={item.category}
+          rating={item.rating}
+          reviewCount={item.reviewCount}
+        />
+      ))}
+    </div>
+  );
 }
 
-function BusinessAddress() {
-    return <p className="Business-address">{address}</p>
-}
+export default BusinessList;
 
-function BusinessPhone() {
-    return <p className="Business-phone">{phone}</p>
-}
 
-function BusinessCategory() {
-    return <p className="Business-category">Category: {category}</p>
-}
 
-function BusinessRating() {
-    return <p className="Business-rating">Rating on Yelp: {rating}</p>
-}    
-
-function BusinessReviewCount() {
-    return <p className="Business-reviewCount"> Reviews: ({reviewCount})</p>
-}   
-
-function Businesses() {
-    return (
-        <div className='Business-Card'>
-            <li>
-            <BusinessImage alt='inside of the resturant'/>
-            <BusinessNames />
-            <BusinessAddress />
-            <BusinessPhone />
-            <BusinessCategory />
-            <BusinessRating />
-            <BusinessReviewCount />  
-            </li>
-        </div>
-    )
-};
-
-export default Businesses; 
